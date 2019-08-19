80cb19b03912b90ea199e33f3d0d6d18123f5060
PawilIOS <wileypatrick36@gmail.com>
2019-08-19 11:44:44 +0200

    create QuoteService file/Class

---
+++ ClassQuote/Model/QuoteService.swift
@@ -0,0 +1,15 @@
+//
+//  QuoteService.swift
+//  ClassQuote
+//
+//  Created by Patrick Wiley on 19.08.19.
+//  Copyright © 2019 Patrick Wiley. All rights reserved.
+//
+
+import Foundation
+
+class QuoteService {
+    
+    
+    
+}
